3089c606b505507d74180620cb0d6c0f15b3dcf0
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-03 11:25:31 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+const record = [
+    {year: "2024", result: "L"},
+    {year: "2023", result: "L"},
+    {year: "2022", result: "N/A"},
+    {year: "2021", result: "L"},
+    {year: "2020", result: "N/A"},
+    {year: "2019", result: "N/A"},
+    {year: "2018", result: "N/A"},
+    {year: "2017", result: "N/A"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"},
+    {year: "2014", result: "L"},
+    {year: "2013", result: "N/A"},
+    {year: "2012", result: "L"},
+    {year: "2011", result: "L"},
+    {year: "2010", result: "L"},
+]
+
+function superbowlWin(record){
+    const winningRecord = record.find((item) => item.result === 'W');
+    if(!winningRecord){
+        return undefined
+    } else {
+        return winningRecord.year
+    }
+}
+
